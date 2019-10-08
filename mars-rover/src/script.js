96044b167b657d452cd4c717109a949c60084055
alexis <alexis@DESKTOP-AJ1CK7K.localdomain>
2019-10-08 19:24:18 +0100

    second commit

--- mars-rover/src/script.js
+++ mars-rover/src/script.js
@@ -51,29 +51,33 @@ function turnRight(rover){
 }
 
 function moveForward(rover){
- 
+let a;
 let moved = true;
   switch(rover.direction){
     case "N":
-    if (rover.x-1 >=0){
+    a = map[rover.x-1][rover.y];
+    if ((rover.x-1 >=0) && (a !='w')){
     rover.x--;
     }else
     moved = false;
     break;
     case "W":
-    if (rover.y-1 >=0 ){
+    a = map[rover.x][rover.y-1];
+    if ((rover.y-1 >=0 )&&(a !='w')){
     rover.y--;
     }else
     moved = false;
     break;
     case "S":
-    if (rover.x+1 <=10){
+    a = map[rover.x+1][rover.y];
+    if ((rover.x+1 <=10)&&(a !='w')){
     rover.x++;
     }else
     moved = false;
     break;
     case "E":
-    if (rover.y+1 <=10){
+    a = map[rover.x][rover.y+1];
+    if ((rover.y+1 <=10) && (a !='w')){
     rover.y++;
     }else
     moved = false;
@@ -83,52 +87,56 @@ let moved = true;
   let newPosition = {x: rover.x, y:rover.y};
   rover.path.push(newPosition);
   console.log('rover moved forward');
-  }else
-  console.log('rover cannot move');
-
+ }else if (a == 'w')
+    console.log('rover cannot move through');
+    else
+    console.log('rover cant move');
   }
 
 
-  
-
-  
 
   function moveBackward(rover){
- 
 
-  let moved = true;
-  switch(rover.direction){
-    case "N":
-    if (rover.x+1 <=10){
-    rover.x++;
-    }else
-    moved = false;
-    break;
-    case "W":
-    if (rover.y+1 <=10 ){
-    rover.y++;
-    }else
-    moved = false;
-    break;
-    case "S":
-    if (rover.x-1 >=0){
-    rover.x--;
-    }else
-    moved = false;
-    break;
-    case "E":
-    if (rover.y-1 >=0){
-    rover.y--;
-    }else
-    moved = false;
-    break;
-  }
-  if (moved === true){
-  let newPosition = {x: rover.x, y:rover.y};
-  rover.path.push(newPosition);
-  console.log('rover moved back');
-  }else
-  console.log('rover cannot move');
+    let a;
+    let moved = true;
+    switch(rover.direction){
+      case "N":
+      a =  map[rover.x+1][rover.y];
+      if ((rover.x+1 <=10)&&(a !='w')){
+      rover.x++;
+      }else
+      moved = false;
+      break;
+      case "W":
+      a = map[rover.x][rover.y+1];
+      if ((rover.y+1 <=10 )&&(a!='w')){
+      rover.y++;
+      }else
+      moved = false;
+      break;
+      case "S":
+      a = map[rover.x-1][rover.y];
+      if ((rover.x-1 >=0)&&(a !='w')){
+      rover.x--;
+      }else
+      moved = false;
+      break;
+      case "E":
+      a = map[rover.x][rover.y-1];
+      if ((rover.y-1 >=0)&&(a !='w')){
+      rover.y--;
+      }else
+      moved = false;
+      break;
+    }
+    if (moved === true){
+    let newPosition = {x: rover.x, y:rover.y};
+    rover.path.push(newPosition);
+    console.log('rover moved back');
+    }else if (a == 'w')
+    console.log('rover cannot move through');
+    else
+    console.log('rover cant move');
 
   }
   
@@ -158,7 +166,21 @@ function orders (rover,commands){
      console.log(rover.path);
 }
 
+const map = [
+  ['','w','','','','','','','','',],
+  ['','w','','','','','','','','',],
+  ['','w','w','','','','','','','',],
+  ['','w','w','w','','','','','','',],
+  ['','','','','','','w','','','',],
+  ['','','','','','','','','','',],
+  ['','','','','','','','','','',],
+  ['','','','','','','','','','',],
+  ['','','','','','','','','','',],
+  ['','','','','','','','','','',]
+];
 
+map[marsRover.x][marsRover.y] = 'z';
+
+console.log(map);
   
-//orders(marsRover, "rfabc");
-
+//orders(marsRover, "lb");
